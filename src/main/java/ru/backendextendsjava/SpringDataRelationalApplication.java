78cf6214fb30b7d4a235ed32a14b1f52b3574976
Dmitry Smirnov <dmitry.vad.smirnov@tele2.ru>
2025-03-10 18:11:37 +0300

    init commit

---
+++ src/main/java/ru/backendextendsjava/SpringDataRelationalApplication.java
@@ -0,0 +1,11 @@
+package ru.backendextendsjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRelationalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataRelationalApplication.class, args);
+    }
+}
